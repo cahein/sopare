6e269246444970d91d5083a81f071a0bc7dedf96
Carsten Heinrigs <carsten@cahein.de>
2022-02-01 21:27:58 +0100

    handle connect failure

--- plugins/recorder/ecasound_handler.py
+++ plugins/recorder/ecasound_handler.py
@@ -26,24 +26,22 @@ class EcasoundHandler:
         self.ecaiam.command('c-add record-chain')
         self.ecaiam.command('ai-add alsa')
         self.ecaiam.command('cop-add -ea:200')
-        # self.ecaiam.command('cs-set-audio-format 16,2,44100')
+        self.ecaiam.command('cs-set-audio-format 16,1,44100')
 
     def play_sound(self, soundfile):
-        self.logger.debug('prepare to play ' + soundfile)
-
         current_selected = self.get_selected_chainsetup()
-        self.logger.debug('currently selected chainsetup: ' +
-                          current_selected)
-        if (current_selected == 'record-chainsetup'):
-            self.stop_recording()
+        if current_selected == 'record-chainsetup':
+            status = self.get_engine_status()
+            self.logger.debug('record-chainsetup engine-status: ' + status)
 
         self.ecaiam.command('cs-select play-chainsetup')
         self.ecaiam.command('c-select play-chain')
         self.ecaiam.command('ai-add ' + soundfile)
 
-        connected = self.ecaiam.command('cs-connect')
-        self.logger.info('chainsetup connected? ' + str(connected))
-
+        connect_error = self.ecaiam.command('cs-connect')
+        if connect_error:
+            self.logger.error('chainsetup connection error: ' + str(connect_error))
+            return
         self.ecaiam.command('start')
 
         self.ecaiam.command('cs-get-length')
@@ -52,27 +50,27 @@ class EcasoundHandler:
 
         while 1:
             time.sleep(1)
-            self.ecaiam.command('engine-status')
-            status = self.ecaiam.last_string()
-            self.logger.debug('engine status: ' + status)
 
+            status = self.get_engine_status()
             if status == 'not started' or status == 'finished':
                 self.ecaiam.command('stop')
                 self.ecaiam.command('ai-remove')
                 self.ecaiam.command('cs-disconnect')
                 self.logger.info('chainset disconnected')
                 return length
-                break
-
+        
     def start_recording(self, filename):
         self.ecaiam.command('cs-select record-chainsetup')
         self.ecaiam.command('c-select record-chain')
         self.ecaiam.command('cs-set-position 0')
         self.ecaiam.command('cs-set-length 1800')
 
-        self.logger.info('recording to ' + filename)
         self.ecaiam.command('ao-add ' + filename)
-        self.ecaiam.command('cs-connect')
+
+        connect_error = self.ecaiam.command('cs-connect')
+        if connect_error:
+            self.logger.error('chainsetup connection error: ' + str(connect_error))
+            return
         self.ecaiam.command('start')
 
     def stop_recording(self):
@@ -84,17 +82,17 @@ class EcasoundHandler:
     def get_engine_status(self):
         self.ecaiam.command('engine-status')
         status = self.ecaiam.last_string()
-        self.logger.debug('engine-status:' + status)
+        self.logger.debug('engine-status: ' + status)
         return status
 
     def get_connected_chainsetup(self):
         self.ecaiam.command('cs-connected')
         cs_connected = self.ecaiam.last_string()
-        self.logger.debug('connected chainsetup:' + cs_connected)
+        self.logger.debug('connected chainsetup: ' + cs_connected)
         return cs_connected
 
     def get_selected_chainsetup(self):
         self.ecaiam.command('cs-selected')
         cs_selected = self.ecaiam.last_string()
-        self.logger.debug('selected chainsetup:' + cs_selected)
+        self.logger.debug('selected chainsetup: ' + cs_selected)
         return cs_selected
